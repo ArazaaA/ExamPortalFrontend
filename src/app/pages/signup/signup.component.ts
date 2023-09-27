d105596185b8dd735ce6e14cc648c96d9febe3df
origin <aamirraza8987@gmail.com>
2023-09-28 02:27:42 +0530

    Data Bind

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit{
+  constructor() {}
+
+  public user = {
+   userName: '',
+   password: '',
+   firstName: '',
+   lastName: '',
+   email: '',
+   phone: '',
+
+  };
+
+  ngOnInit(): void {}
+
+  formSubmit() {
+    console.log(this.user);
+    if(this.user.userName=='' || this.user.userName == null)
+    {
+      alert('User is Required !!');
+      return;
+    }
+  }
+
+
+}
